186424fd8059e9114932942c2720710fdc74129a
sourav chakraborty <sourav@turbot.com>
2023-05-26 14:35:40 +0530

    Update auth cache to use go-generics-cache

--- go.mod
+++ go.mod
@@ -1,9 +1,14 @@
 module github.com/turbot/zoom-lib-golang
 
-go 1.13
+go 1.18
+
+require (
+	github.com/Code-Hex/go-generics-cache v1.3.1
+	github.com/google/go-querystring v1.0.0
+	gopkg.in/dgrijalva/jwt-go.v3 v3.2.0
+)
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/google/go-querystring v1.0.0
-	gopkg.in/dgrijalva/jwt-go.v3 v3.2.0
+	golang.org/x/exp v0.0.0-20220328175248-053ad81199eb // indirect
 )
--- oauth.go
+++ oauth.go
@@ -1,6 +1,7 @@
 package zoom
 
 import (
+	"context"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
@@ -9,14 +10,9 @@ import (
 	"net/url"
 	"strings"
 	"time"
+
+	cache "github.com/Code-Hex/go-generics-cache"
 )
-
-var tokenCache = make(map[string]tokenInfo)
-
-type tokenInfo struct {
-	Token          string
-	ExpirationTime time.Time
-}
 
 type AccessTokenResponse struct {
 	AccessToken string `json:"access_token"`
@@ -24,12 +20,14 @@ type AccessTokenResponse struct {
 }
 
 func OAuthToken(accountID string, clientID string, clientSecret string) (string, error) {
-	tokenData, exists := tokenCache[accountID+clientID+clientSecret]
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+  c := cache.NewContext[string, string](ctx)
 
-	if exists && !tokenData.ExpirationTime.Before(time.Now()) {
-		return tokenData.Token, nil
+	if token, ok := c.Get("accountID+clientID+clientSecret"); ok {
+		return token, nil
 	}
-
+  fmt.Println("test cache")
 	data := url.Values{}
 	data.Set("grant_type", "account_credentials")
 	data.Set("account_id", accountID)
@@ -67,13 +65,9 @@ func OAuthToken(accountID string, clientID string, clientSecret string) (string,
 	}
 
 	// set the expiration time for the token to be 5 minutes less than the actual expiry time
-	expirationTime := time.Now().Add(time.Second * time.Duration(accessTokenResp.ExpiresIn-300))
-	tokenData = tokenInfo{
-		Token:          accessTokenResp.AccessToken,
-		ExpirationTime: expirationTime,
-	}
+	expirationTime := time.Duration(accessTokenResp.ExpiresIn-300)
 
-	tokenCache[accountID+clientID+clientSecret] = tokenData
+	c.Set("accountID+clientID+clientSecret", accessTokenResp.AccessToken, cache.WithExpiration(expirationTime))
 
 	return accessTokenResp.AccessToken, nil
 }
